00acc7afffd0ece50638805dc5e93314eb8c9dbc
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2021-11-08 12:50:55 -0500

    ip fix

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const myip = require('quick-local-ip');
+
 
 let eth_address = "null";
 
@@ -10,26 +12,9 @@ app.get('/set_address', (req, res) => {
 });
 
 app.get('/ip', (req, res) => {
-    'use strict';
-
-    const { networkInterfaces } = require('os');
     
-    const nets = networkInterfaces();
-    const results = Object.create(null); // Or just '{}', an empty object
-    
-    for (const name of Object.keys(nets)) {
-        for (const net of nets[name]) {
-            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-            if (net.family === 'IPv4' && !net.internal) {
-                if (!results[name]) {
-                    results[name] = [];
-                }
-                results[name].push(net.address);
-            }
-        }
-    }
 
-    res.send(results["Wi-Fi"][0]);
+    res.send(myip.getLocalIP4());
 });
 
 app.get('/address', (req, res) => {
