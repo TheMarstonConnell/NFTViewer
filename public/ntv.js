b05341a443d9adb54201affccb697712a7b1a180
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2021-11-08 12:44:01 -0500

    verify stuff

--- public/ntv.js
+++ public/ntv.js
@@ -5,7 +5,20 @@ function main(){
       }).then(function(data) {
         let text = document.createElement('h1');
         text.innerHTML = "VISIT: " + data + ":3000/verify.html on a device with MetaMask installed to get your NFTs on this screen.";
+
+        let timer = document.createElement('h2');
         document.body.appendChild(text);
+        document.body.appendChild(timer);
+
+        let x = 10;
+        setInterval(() => {
+            timer.innerHTML = "This page will refresh in " + x + " seconds..."
+            x -= 1;
+        }, 1000);
+
+        setTimeout(() => {
+            window.location.href = "/";
+        }, 10 * 1000);
       }).catch(function() {
     
     });
